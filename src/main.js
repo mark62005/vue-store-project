f392bea09dcf053f51b3c052940dd090891e5e00
mark62005 <work.mark62005@gmail.com>
2020-12-10 19:35:40 +0000

    basic setup + get API

--- src/main.js
+++ src/main.js
@@ -11,7 +11,10 @@ Vue.use(VueAxios, axios);
 new Vue({
   router,
   created() {
-
+    const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/products`;
+    this.axios.get(api)
+      .then(() => console.log('GET DATA SUCCESS'))
+      .catch(() => console.log('ERROR!!!!'));
   },
   render: (h) => h(App),
 }).$mount('#app');
