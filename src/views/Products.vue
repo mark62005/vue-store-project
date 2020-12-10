eb5c1a3f9ee5ca489d9101e242241696bff8e9ac
mark62005 <work.mark62005@gmail.com>
2020-12-10 22:33:17 +0000

    create admin products list

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,11 +1,70 @@
 <template>
   <div>
-    123
+    <div class="text-right mt-4">
+      <button class="btn btn-primary">建立新的產品</button>
+    </div>
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th width="120">分類</th>
+          <th>產品名稱</th>
+          <th width="120">原價</th>
+          <th width="120">售價</th>
+          <th width="100">是否已啟用</th>
+          <th width="80">編輯</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="product in products"
+          :key="product.id"
+        >
+          <td>{{ product.category }}</td>
+          <td>{{ product.title }}</td>
+          <td class="text-right">
+            {{ product.origin_price }}
+          </td>
+          <td class="text-right">
+            {{ product.price }}
+          </td>
+          <td>
+            <span
+              v-if="product.is_enabled"
+              class="text-success"
+            >已啟用</span>
+            <span v-else>未啟用</span>
+          </td>
+          <td>
+            <button class="btn btn-outline-primary btn-sm">
+              編輯
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Products',
+  data() {
+    return {
+      products: undefined,
+    };
+  },
+  async created() {
+    this.getProducts();
+  },
+  methods: {
+    getProducts() {
+      const api = `${process.env.VUE_APP_API_PATH}/api/${process.env.VUE_APP_CUSTOM_PATH}/admin/products`;
+      this.axios.get(api).then((res) => {
+        this.products = res.data.products;
+      }).catch((res) => {
+        console.log(res.data);
+      });
+    },
+  },
 };
 </script>
