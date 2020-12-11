76eeea4f5085d5b99cfb108d542b13a5e8bb4f20
mark62005 <work.mark62005@gmail.com>
2020-12-11 03:18:23 +0000

    create Coupons & Orders & adjust Sidebar.vue style

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Dashboard from '../views/Dashboard.vue';
 import Products from '../views/Products.vue';
+import Orders from '../views/Orders.vue';
+import Coupons from '../views/Coupons.vue';
 
 Vue.use(VueRouter);
 
@@ -23,6 +25,20 @@ const routes = [
         path: 'products',
         name: 'products',
         component: Products,
+        meta: { requiresAuth: true },
+      },
+
+      {
+        path: 'orders',
+        name: 'orders',
+        component: Orders,
+        meta: { requiresAuth: true },
+      },
+
+      {
+        path: 'coupons',
+        name: 'coupons',
+        component: Coupons,
         meta: { requiresAuth: true },
       },
     ],
---
+++ src/views/Coupons.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Coupons
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Coupons',
+};
+</script>
